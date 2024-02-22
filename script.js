cebe8205fc858ca58c748c5346db5937a9539c87
abifol <abifol479@gmail.com>
2024-02-22 08:13:51 +0100

    pushing cosmetics

--- script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $("marquee")
-    .mouseover(function () {
-      $(this).attr("scrollamount", 0);
-    })
-    .mouseout(function () {
-      $(this).attr("scrollamount", 5);
-    });
-});
