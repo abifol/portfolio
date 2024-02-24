869c3b90c45c76db072add291977883c732b1b5e
abifol <abifol479@gmail.com>
2024-02-24 21:23:57 +0100

    pushing fix to hide navbar onscroll

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// This is a javscript to hide the navbar on scroll
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.bottom = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.bottom = "-90px";
   }
   prevScrollpos = currentScrollPos;
 };
